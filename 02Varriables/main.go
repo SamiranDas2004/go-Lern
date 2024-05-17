0c08fe65f351ac454ad29ca982f78e11eb08ed97
Samiran_Das <samiran4209@gmail.com>
2024-05-18 00:55:20 +0530

    go variables

---
+++ 02Varriables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var usename string = "sam"
+	// println(usename)
+	// fmt.Printf("hello %T", usename)
+
+	var isLoggedin bool = true
+	println(isLoggedin)
+	fmt.Printf("hello %T", isLoggedin)
+}
